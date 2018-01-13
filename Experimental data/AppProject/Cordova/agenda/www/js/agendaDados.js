e674c4651d7a1d56c5696dee5a049cbe290d4c16
André Augusto Menegassi <andremenegassi@hotmail.com>
2018-01-12 22:57:34 -0200

    Delete

--- Experimental data/AppProject/Cordova/agenda/www/js/agendaDados.js
+++
@@ -1,9 +0,0 @@
-define(function(require){
-    "use strict";
-    var PersistenciaDados = require("./PersistenciaDados");
-    var agendaDados = new PersistenciaDados({
-        key: "_agenda_dados.json"
-    });
-
-    return agendaDados;
-});
--- Experimental data/AppProject/Cordova/orderapp/README.md
+++
@@ -1,4 +0,0 @@
-PedidApp
-=======
-
-App de exemplo do livro de Cordova da Casa do Código usando Ionic 1.x.
